2ae0a69e30e97963c30ca446a17df96e891b52a3
WanomiR <wanomir@yandex.ru>
2023-10-27 17:20:07 +0300

    feat: upload RAW and download JPG

--- src/app.py
+++ src/app.py
@@ -1,5 +1,34 @@
+from PIL import Image
+import streamlit as st
 import rawpy
-import numpy as np
+import re
+from io import BytesIO
 
-INPUT_PATH = "samples/DSC_1967.nef"
-NOISE_SAMPLE_PATH = "samples/kodak-portra.npy"
+st.title("Image Converter")
+
+uploaded_file = st.file_uploader("Upload a raw image file")
+
+if uploaded_file is not None:
+
+    file_name = re.search(
+        "[\S]*(?=\.)",
+        uploaded_file.name
+    ).group()
+
+    with rawpy.imread(uploaded_file) as raw:
+        rgb = raw.postprocess(use_camera_wb=True)
+
+    st.image(rgb, caption="This is your image!")
+
+    buffer = BytesIO()
+    Image.fromarray(rgb).save(buffer, format="JPEG")
+    img_bytes = buffer.getvalue()
+
+    st.download_button(
+        label="Download image",
+        data=img_bytes,
+        file_name=file_name + ".jpg",
+        mime="image/jpeg"
+    )
+
+
